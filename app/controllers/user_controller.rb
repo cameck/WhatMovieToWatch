fdbded2e21a7abc6765c9b948022848c5280bead
Cameron Eckelberry <cameck@gmail.com>
2016-06-26 17:59:33 -0400

    Added profile picture to DB

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,4 +1,5 @@
 class UserController < ApplicationController
   def index
+    @watchlist_items = current_user.watchlist_items
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160626211616) do
+ActiveRecord::Schema.define(version: 20160626215805) do
 
   create_table "users", force: :cascade do |t|
     t.string   "provider"
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 20160626211616) do
     t.datetime "oauth_expires_at"
     t.datetime "created_at",       null: false
     t.datetime "updated_at",       null: false
+    t.string   "profile_picture"
   end
 
   create_table "watchlist_items", force: :cascade do |t|
