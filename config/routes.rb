0a53b05f6eb3cfdf15553eb71d9fac4ce6762dd1
Cameron Eckelberry <cameck@gmail.com>
2016-06-26 15:41:57 -0400

    Added omniauth intializer and routes for creating and deleting a user session

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
-
+  
+  get 'auth/:provider/callback', to: 'sessions#create'
+  get 'auth/failure', to: redirect('/')
+  get 'signout', to: 'sessions#destroy', as: 'signout'
 
   resources :movie
   root 'movie#index'
