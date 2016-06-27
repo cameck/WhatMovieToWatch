651ecc46c1d29f541e4b40e547d57a2425ebf08c
Cameron Eckelberry <cameck@gmail.com>
2016-06-27 15:59:23 -0400

    Fixed small mistake on redirect if user not signed in

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -2,7 +2,8 @@ class UserController < ApplicationController
   def index
     if !current_user
       redirect_to root_path
+    else
+      @watchlist_items ||= current_user.watchlist_items
     end
-    @watchlist_items ||= current_user.watchlist_items
   end
 end
