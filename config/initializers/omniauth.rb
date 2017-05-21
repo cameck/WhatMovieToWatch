f974a50254d50442a5e44e551e769beba0942baa
Cameron Eckelberry <cameck@gmail.com>
2017-05-21 16:46:45 -0400

    dirty witchcraft

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,8 +1,3 @@
-OmniAuth.config.logger = Rails.logger
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :facebook,
-           ENV['FACEBOOK_KEY'],
-           ENV['FACEBOOK_SECRET'],
-           display: 'popup',
-           image_size: 'large'
+  provider :facebook, ENV['FACEBOOK_KEY'], ENV['FACEBOOK_SECRET'], display: 'popup', image_size: 'large'
 end
