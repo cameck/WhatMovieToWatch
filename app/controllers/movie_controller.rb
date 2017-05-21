b3859ce1dcc8f281d92f4f0302cb696c4eb56a5f
Cameron Eckelberry <cameck@gmail.com>
2017-05-21 17:00:03 -0400

    some refactoring

--- app/controllers/movie_controller.rb
+++ app/controllers/movie_controller.rb
@@ -1,5 +1,4 @@
 class MovieController < ApplicationController
-
   def index
     @genres ||= Movie.genres
   end
@@ -8,21 +7,24 @@ class MovieController < ApplicationController
     @movie_results ||= Movie.new
     @genre = Movie.find_genre(params[:id])
     @movie_results = @movie_results.get_movie_results(params[:id])
-    @movie_results = @movie_results["results"]
+    @movie_results = @movie_results['results']
 
-    if current_user
-      @watchlist_items ||= current_user.watchlist_items
-      @seen_movies ||= current_user.seen_movies
-      seen_movie_titles = []
-
-      @seen_movies.each do |movie|
-        seen_movie_titles << movie.movie_title
-      end
-
-      @movie_results.delete_if {|h| seen_movie_titles.include? h["title"]}
-    end
+    seem_movie_gen if current_user
   end
 
-  def about
+  def about; end
+
+  private
+
+  def seem_movie_gen
+    @watchlist_items ||= current_user.watchlist_items
+    @seen_movies ||= current_user.seen_movies
+    seen_movie_titles = []
+
+    @seen_movies.each do |movie|
+      seen_movie_titles << movie.movie_title
+    end
+
+    @movie_results.delete_if { |h| seen_movie_titles.include? h['title'] }
   end
 end
