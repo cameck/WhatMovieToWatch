ac5224c7f0d8f676da017428f1cf89b1440ef947
Cameron Eckelberry <cameck@gmail.com>
2017-05-21 16:41:15 -0400

    add omniauth gem explicitly

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,4 @@
 OmniAuth.config.logger = Rails.logger
-puts "HERE IS THE FACEBOOK KEY!: #{ENV['FACEBOOK_KEY']}"
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :facebook,
            ENV['FACEBOOK_KEY'],
