3350dc08a2794507cb29f4d566a93f3798ea0427
Cameron Eckelberry <cameck@gmail.com>
2017-05-21 15:43:51 -0400

    no json

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -7,8 +7,5 @@ Rails.application.config.middleware.use OmniAuth::Builder do
            client_options: {
              site: 'https://graph.facebook.com/v2.4',
              authorize_url: 'https://www.facebook.com/v2.4/dialog/oauth'
-           },
-           token_params: {
-             parse: :json
            }
 end
