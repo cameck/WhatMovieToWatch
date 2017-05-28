6c841e034b1f7e1187ebb7dbd7c694b6460e428e
Cameron Eckelberry <cameck@gmail.com>
2017-05-28 18:25:04 -0400

    fix over https error

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -12,10 +12,16 @@ class UserController < ApplicationController
     user = User.find_by(uid: params[:id])
     if user
       @user_name = user.name
-      @profile_pic = user.profile_picture
+      @profile_pic = serve_over_https(user.profile_picture)
       @public_watchlist_items = user.watchlist_items
     else
       redirect_to root_path
     end
+  end
+
+  private
+
+  def serve_over_https(pic)
+    pic.include?('http:') ? pic.sub('http', 'https') : pic
   end
 end
