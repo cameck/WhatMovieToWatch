6b6d6450b61f0dadf3b8fdba68dd4af7c7bc046d
Cameron Eckelberry <cameck@gmail.com>
2017-05-28 18:16:53 -0400

    some cleanup

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,31 +1,30 @@
 class Movie
-
   def get_movie_results(genre_id)
-    movie_results = HTTParty.get("https://api.themoviedb.org/3/discover/movie?with_genres=#{genre_id}&api_key=#{ENV['MOVIE_API_KEY']}&sort_by=popularity.desc")
+    movie_results = HTTParty.get("http://api.themoviedb.org/3/discover/movie?with_genres=#{genre_id}&api_key=#{ENV['MOVIE_API_KEY']}&sort_by=popularity.desc")
     JSON.parse(movie_results.body)
   end
 
   def self.genres
-    return genres = {
-      28 => "Action",
-      12 => "Adventure",
-      16 => "Animation",
-      35 => "Comedy",
-      80 => "Crime",
-      18 => "Drama",
-      10751 => "Family",
-      14 => "Fantasy",
-      10769 => "Foreign",
-      36 => "History",
-      27 => "Horror",
-      10402 => "Music",
-      9648 => "Mystery",
-      10749 => "Romance",
-      878 => "Science Fiction",
-      10770 => "TV Movie",
-      53 => "Thriller",
-      10752 => "War",
-      37 => "Western",
+    {
+      28 => 'Action',
+      12 => 'Adventure',
+      16 => 'Animation',
+      35 => 'Comedy',
+      80 => 'Crime',
+      18 => 'Drama',
+      10751 => 'Family',
+      14 => 'Fantasy',
+      10769 => 'Foreign',
+      36 => 'History',
+      27 => 'Horror',
+      10402 => 'Music',
+      9648 => 'Mystery',
+      10749 => 'Romance',
+      878 => 'Science Fiction',
+      10770 => 'TV Movie',
+      53 => 'Thriller',
+      10752 => 'War',
+      37 => 'Western'
     }
   end
 
